6820f516b9f9a839b4b0aa8460c52b42e138fb27
Anton De Jaeger <antondejaeger@waylay.io>
2025-10-30 14:28:10 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '*/dev-website-antondj.com/',
+  base: 'https://antondj5.github.io/dev-website-antondj.com/assets/index-DTvDL3ps.js',
   server: {
     port: 3000,
     open: true
